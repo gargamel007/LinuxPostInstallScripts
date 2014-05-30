99abf1ead7fa2b23642c4f2bdfe2cf7252c10dd3
gargamel007 <gargamel@m4tp.org>
2014-05-30 22:52:58 +0200

    More programs

--- SimpleXubuntuDesktop.sh
+++ SimpleXubuntuDesktop.sh
@@ -33,21 +33,28 @@ echo "UPGRADE  && INSTAL BASE TOOLS"
 apt-get -qq update && apt-get -qq -y upgrade
 #tree lists contents of a directory
 apt-get install -y -qq tree vim less screen git htop mosh openssh-server terminator sshfs gksu leafpad synaptic gdebi linux-firmware-nonfree gparted
-apt-get install -y -qq vlc aptitude synaptic gdebi-core gnome-system-monitor xubuntu-restricted-extras libavcodec-extra
+apt-get install -y -qq vlc aptitude synaptic gdebi-core gnome-system-monitor xubuntu-restricted-extras libavcodec-extra ttf-mscorefonts-installer non-free-codecs
+#speedup app startup time
+apt-get install -y -qq preload zram-config
+#Optionnal : shotwell (photo organizer), clementine (music player)
 
 #Setup a proper ssh server
 sed -i "s/PermitRootLogin yes/PermitRootLogin no/g" /etc/ssh/sshd_config
 rm /etc/ssh/ssh_host_*
 dpkg-reconfigure openssh-server
 
-##Fix blank screen on resume Bug :(
-sudo add-apt-repository ppa:xubuntu-dev/ppa
-sudo apt-get update && sudo apt-get install xfce4-power-manager light-locker-settings
+##Fix blank screen on resume Bug in Xubuntu :(
+#sudo add-apt-repository ppa:xubuntu-dev/ppa
+#sudo apt-get update && sudo apt-get install xfce4-power-manager light-locker-settings
 
+#For laptops
+add-apt-repository ppa:linrunner/tlp
+apt-get update
+apt-get install tlp tlp-rdw
 
 tee -a /etc/sysctl.conf 1>/dev/null <<END
 # Decrease swap usage to a reasonable level
-vm.swappiness=10
+vm.swappiness=5
 # Improve cache management
 vm.vfs_cache_pressure=50
 END

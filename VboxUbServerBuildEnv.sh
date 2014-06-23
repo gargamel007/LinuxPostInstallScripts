ae4d646b5aedef4038286c59feee37386b9e011b
gargamel007 <gargamel@m4tp.org>
2014-06-23 11:31:30 +0200

    fixing locales

--- VboxUbServerBuildEnv.sh
+++ VboxUbServerBuildEnv.sh
@@ -7,7 +7,7 @@
 :<<'USAGE'
 sudo apt-get -qq update && sudo apt-get -y -qq upgrade && sudo apt-get -y -qq install git
 git clone https://github.com/gargamel007/LinuxPostInstallScripts.git Code/LinuxPostInstallScripts
-sudo bash Code/LinuxPostInstallScripts/vboxUbServerBuildEnv.sh
+sudo bash Code/LinuxPostInstallScripts/VboxUbServerBuildEnv.sh
 USAGE
 
 ###########################
@@ -29,8 +29,9 @@ export DEBIAN_FRONTEND=noninteractive
 set -x
 
 #Fix locale issue
-sed -i "s/# fr_FR.UTF-8/fr_FR.UTF-8/g" /etc/locale.gen
-sed -i "s/# en_US.UTF-8/en_US.UTF-8/g" /etc/locale.gen
+/usr/share/locales/install-language-pack fr_FR
+/usr/share/locales/install-language-pack en_US
+locale-gen --purge fr_FR.UTF-8 en_US.UTF-8
 if [ ! -f /tmp/is_reset_locale ]; then dpkg-reconfigure locales; fi
 touch /tmp/is_reset_locale
 update-locale
